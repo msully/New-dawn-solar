83fc42a8860c04ab4f00eac22e1f6d71993265e1
msully <>
2019-11-29 14:18:02 +0000

    Update Sector header image and benefits layour

--- src/templates/Sector.vue
+++ src/templates/Sector.vue
@@ -1,17 +1,21 @@
 <template>
   <Layout>
-    <article class="relative bg-gray-100 px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-16 lg:py-24">
-      <div class="flex flex-col lg:flex-row mt-24">
+    
+    <article class="relative bg-gray-100">
+      <g-image 
+        :src="$page.sector.cover_image"
+        :alt="$page.sector.title"
+        class="w-full mt-24 h-64 object-cover"
+        />
+      <div class="px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-16">
+
+      <div class="flex flex-col lg:flex-row">
         <div class="w-full lg:px-8">
-          <g-image 
-            :src="$page.sector.cover_image"
-            :alt="$page.sector.title"
-            class="w-full h-48 object-cover"
-            />
+          
           <h1 class="text-4xl font-bold leading-tight mt-4">{{ $page.sector.title }}</h1>
-          <div v-for="item in $page.sector.benefits_list.benefit" :key="item.heading" class="w-full lg:w-1/2 lg:px-8 mt-12 lg:mt-8">
-          <ul class="text-lg sm:text-xl">
-            <li class="checkmark mb-6">
+          <div class="lg:px-8 mt-12 lg:mt-8">
+          <ul class="flex flex-row flex-wrap text-lg sm:text-xl">
+            <li v-for="item in $page.sector.benefits_list.benefit" :key="item.heading" class="w-full md:w-1/2 checkmark mb-6">
               <h3 class="bold mt-2">
                 {{ item.heading }}
               </h3>
@@ -21,11 +25,13 @@
             </li>
           </ul>
           </div>
+          <div class="markdown-body mb-8 mt-2" v-html="$page.sector.description" />
           <div class="markdown-body mb-8 mt-2" v-html="$page.sector.content" />
           <div class="mb-8">
             <g-link to="/sectors" class="font-bold uppercase">Back to Market sectors we support</g-link>
           </div>
         </div>
+      </div>
       </div>
     </article>
   </Layout>
@@ -40,6 +46,7 @@ query Sector ($path: String) {
     cover_image
     published
     description
+    content
     benefits_list {
       heading
       subheading
