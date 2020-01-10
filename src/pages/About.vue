fb9dad70c3c24211364eafce58e89b225a336c97
msully <>
2020-01-10 12:08:54 +0000

    Update About

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,8 +4,8 @@
       <article class="relative bg-gray-100 px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-16 lg:py-24">
         <div class="flex flex-col lg:flex-row mt-6">
           <div class="w-full lg:px-8">
-            <h1 class="text-3xl leading-tight font-bold mt-4">{{ $page.title }}</h1>
-            <div class="markdown" v-html="$page.content"></div>
+            <h1 class="text-3xl leading-tight font-bold mt-4">{{ $page.about.title }}</h1>
+            <div class="markdown" v-html="$page.about.content"></div>
           </div>
         </div>
       </article>
@@ -14,12 +14,11 @@
 </template>
 
 <page-query>  
-query {
-  about(path: "/about/") {
-    id
-    path
+
+query  {
+  about: about (path: "/content/about/") {
     title
-    content  
+    content
   }
 }
 </page-query>
