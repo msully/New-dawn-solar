10506620229e5fe8d2a46b04951aabaf1160d0e1
msully <>
2020-01-11 06:30:39 +0000

    update config remove published tag and add alt

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,6 +4,11 @@
       <article class="relative bg-gray-100 px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-16 lg:py-24">
         <div class="flex flex-col lg:flex-row mt-6">
           <div class="w-full lg:px-8">
+            <g-image 
+            :src="$page.about.cover_image"
+            :alt="$page.project.title"
+            class="w-full h-48 object-cover"
+            />
             <h1 class="text-3xl leading-tight font-bold mt-4">{{ $page.about.title }}</h1>
             <div class="markdown" v-html="$page.about.content"></div>
           </div>
@@ -19,6 +24,7 @@ query  {
   about: about (path: "/content/about/") {
     title
     content
+    cover_image
   }
 }
 </page-query>
