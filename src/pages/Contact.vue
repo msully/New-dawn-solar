a474f50845f7cf7da4b75af69f51317be6faec67
Mike Sullivan <mikesullivan@Mikes-MacBook-Pro-2.local>
2020-09-09 13:29:27 +0100

    Update contact form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -80,9 +80,7 @@
                                 v-model="formData.last_name"
                               />
                             </div>
-                          </div>
-
-                          <div class="w-full px-3">
+                            <div class="w-full px-3">
                               <label
                                 class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                 for="grid-email"
@@ -90,16 +88,14 @@
                               <input
                                 class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                 id="grid-email"
-                                type="email"
+                                type="text"
                                 placeholder="Email address"
                                 required
                                 name="email"
                                 v-model="formData.email"
                               />
                             </div>
-                          </div>
-
-                          <div class="w-full px-3">
+                            <div class="w-full px-3">
                               <label
                                 class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                                 for="grid-phone"
@@ -115,6 +111,8 @@
                             </div>
                           </div>
 
+                          </div>
+                          </div>
                           <div class="flex flex-wrap -mx-3 mb-6">
                             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                               <label
