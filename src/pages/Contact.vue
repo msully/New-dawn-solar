c22ea69b6d85abfa977307a44eedb3113b82fe1b
Mike Sullivan <mikesullivan@Mikes-MacBook-Pro-2.local>
2020-09-09 13:05:55 +0100

    Addd Phone and Email to form

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -82,6 +82,39 @@
                             </div>
                           </div>
 
+                          <div class="w-full px-3">
+                              <label
+                                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-email"
+                              >Email Address</label>
+                              <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-email"
+                                type="email"
+                                placeholder="Email address"
+                                required
+                                name="email"
+                                v-model="formData.email"
+                              />
+                            </div>
+                          </div>
+
+                          <div class="w-full px-3">
+                              <label
+                                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                for="grid-phone"
+                              >Phone number</label>
+                              <input
+                                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="grid-phone"
+                                type="text"
+                                placeholder="Phone number"
+                                name="phone"
+                                v-model="formData.phone"
+                              />
+                            </div>
+                          </div>
+
                           <div class="flex flex-wrap -mx-3 mb-6">
                             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                               <label
