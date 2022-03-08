70203d444977de3cb701679b4010a7fb17c610d8
Mike Sullivan <mike.sullivan@leics.gov.uk>
2022-03-08 10:49:36 +0000

    Update projects grid on projects page

--- src/templates/Project.vue
+++ src/templates/Project.vue
@@ -1,22 +1,33 @@
 <template>
   <Layout>
     <article class="relative bg-gray-100">
-      
-      <g-image 
+
+      <g-image
         :src="$page.project.cover_image"
         :alt="$page.project.title"
-        class="w-full mt-24 h-64 object-cover"
+        class="object-cover w-full h-64 mt-24"
         />
-      <div class="px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-16">
+      <div class="px-4 py-16 sm:px-8 lg:px-16 xl:px-40 2xl:px-64">
 
-          <h1 class="text-4xl font-bold leading-tight mt-4">{{ $page.project.title }}</h1>
-          <div class="text-xl text-gray-600 mb-4 mt-2">{{ $page.project.date }}</div>
-          <div class="markdown mb-8 mt-2" v-html="$page.project.content" />
-          <div class="mb-8">
-            <g-link to="/projects" class="font-bold uppercase">Back to Projects</g-link>
+          <h1 class="mt-4 text-4xl font-bold leading-tight">{{ $page.project.title }}</h1>
+          <div class="mt-2 mb-4 text-xl text-gray-600">{{ $page.project.date }}</div>
+          <div class="mt-2 mb-8 markdown" v-html="$page.project.content" />
+          <ul class="grid grid-cols-2 gap-8 mx-auto text-lg lg:grid-cols-4 xlg:grid-cols-6 sm:text-xl">
+            <li v-for="item in $page.project.galleryImages" :key="item.id" class="w-full">
+
+              <g-image
+                :src="item.image"
+                :alt="item.alt"
+                class="object-cover w-full h-full"
+                />
+
+            </li>
+          </ul>
+          <div class="my-8">
+            <g-link to="/projects" class="mb-4 text-sm font-semibold tracking-wider text-orange-600 uppercase border-b-2 border-transparent md:text-base hover:border-orange-800 hover:border-b-2">Back to Projects</g-link>
           </div>
         </div>
-     
+
     </article>
      <InTouch />
   </Layout>
@@ -30,6 +41,10 @@ query Project ($path: String) {
     description
     content
     cover_image
+    galleryImages {
+      image
+      alt
+    }
   }
 }
 </page-query>
